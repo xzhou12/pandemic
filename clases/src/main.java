75030e1a68bb4ac04df584e156f5bc53ea61b85c
AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>
2023-04-11 18:01:17 +0200

    clase menu v1
    primera version de la clase menu donde nos piden crear el menu e incorporar funcionalidad a los botones Informacion, Autores, Version y Salir.

--- clases/src/main.java
+++ clases/src/main.java
@@ -3,8 +3,7 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		menu menu1 = new menu();
 	}
 
 }
--- clases/src/menu.java
+++ clases/src/menu.java
@@ -1,9 +1,69 @@
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.awt.Color;
 
-public class menu {
-	JPanel Pciudades = new JPanel();
-	JButton NuevaPartida, CargarPartida, Informacion, ResumenPuntuaciones, Autores, Version, Salir = new JButton();
+public class menu extends JFrame {
+	public menu() {
+		setTitle("Pandemic");
+		setBounds(100, 100, 1550, 850);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+
+		//panel del menu
+		JPanel Pciudades = new JPanel();
+		Pciudades.setBounds(0, 0, 1550, 850);
+		Pciudades.setBackground(new Color(5, 153, 209));
+		//imagen de fondo
+		ImageIcon Imagen = new ImageIcon("mapa_mundo.png");
+		Pciudades.setLayout(null);
+		JButton NuevaPartida = new JButton();
+		NuevaPartida.setText("Nueva partida");
+		NuevaPartida.setBounds(619, 357, 139, 53);
+		JButton CargarPartida = new JButton();
+		CargarPartida.setText("Cargar partida");
+		CargarPartida.setBounds(619, 421, 150, 50);
+		JButton Informacion = new JButton();
+		Informacion.setText("Informacion");
+		Informacion.setBounds(621, 494, 109, 41);
+		JButton ResumenPuntuaciones = new JButton();
+		ResumenPuntuaciones.setText("Resumen de puntuaciones");
+		ResumenPuntuaciones.setBounds(619, 585, 173, 41);
+		JButton Autores = new JButton();
+		Autores.setText("Autores");
+		Autores.setBounds(630, 730, 150, 50);
+		JButton Version = new JButton();
+		Version.setText("Version");
+		Version.setBounds(50, 730, 250, 50);
+		Pciudades.add(NuevaPartida);
+		Pciudades.add(CargarPartida);
+		Pciudades.add(Informacion);
+		Pciudades.add(ResumenPuntuaciones);
+		Pciudades.add(Autores);
+		Pciudades.add(Version);
+		getContentPane().add(Pciudades);
+		
+		JButton Salir = new JButton();
+		Salir.setText("Salir");
+		Salir.setName("");
+		Salir.setBounds(1320, 730, 150, 50);
+		Pciudades.add(Salir);
+		JLabel MapaMundi = new JLabel(Imagen);
+		MapaMundi.setFocusable(false);
+		MapaMundi.setBounds(0,0,1540,811);
+		//añade la imagen al menu
+		Pciudades.add(MapaMundi);
+		setVisible(true);
+	}
 
 	static void NuevaPartida() {
 
@@ -22,6 +82,7 @@ public class menu {
 	}
 
 	static void Autores() {
+		System.out.println("Autores: Albert Barrachina & Xiaobin Zhou");
 
 	}
 

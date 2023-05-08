1c12c5da03c3edb3f36489c920b68761e21c6ebf
xzhou12 <117665173+xzhou12@users.noreply.github.com>
2023-05-08 18:01:35 +0200

    Acomodar archivos
    Co-Authored-By: AlbertBarrachina <87174156+AlbertBarrachina@users.noreply.github.com>

--- Pandemic/src/jugador.java
+++
@@ -1,5 +0,0 @@
-
-public class jugador {
-	int acciones, porcentaje = 0;
-
-}
--- Pandemic/src/tablero.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
-
-public class tablero {
-	JPanel Pciudades = new JPanel();
-	ImageIcon mapa = new ImageIcon();
-
-}
